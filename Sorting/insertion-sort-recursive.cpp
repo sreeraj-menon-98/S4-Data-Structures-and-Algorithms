40ca372a9b1e7674d4158e9853cab4ad1c0ae07c
Sreeraj Menon <sreerajmenonseeraj@gmail.com>
2019-04-21 11:42:03 +0530

    fix

--- Sorting/insertion-sort-recursive.cpp
+++ Sorting/insertion-sort-recursive.cpp
@@ -19,7 +19,7 @@ int insertion_sort(int array[], int n)
 int main()
 {
 	ios_base::sync_with_stdio(false);cin.tie(NULL);
-	int array[100], n;
+	int array[10000], n;
 	cin>>n;
 	for (int i = 0; i < n; ++i)
 	{
